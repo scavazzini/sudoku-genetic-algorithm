2bc147da4e25694dd05d95d789fa5af287b594b8
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-09-03 18:39:19 -0300

    Add avgFitness in Population

--- src/main/java/Population.java
+++ src/main/java/Population.java
@@ -1,6 +1,7 @@
 public class Population {
 
     private Board[] boards;
+    private double avgFitness;
 
     public Population(int[][] initialBoard, int populationSize) {
         this.boards = new Board[populationSize];
@@ -8,6 +9,23 @@ public class Population {
         for (int i = 0; i < populationSize; i++) {
             this.boards[i] = new Board(initialBoard);
         }
+
+        this.calculateAvgFitness();
     }
 
+    private void calculateAvgFitness() {
+        for (Board board : this.boards) {
+            this.avgFitness += board.getFitness();
+        }
+        this.avgFitness = this.avgFitness / this.boards.length;
+    }
+
+    public Board[] getBoards() {
+        return boards;
+    }
+
+    @Override
+    public String toString() {
+        return "Average fitness: " + this.avgFitness;
+    }
 }
