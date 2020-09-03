e410517e8bff54c18802e0bf2b0137bf0d7d0042
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-09-03 20:11:45 -0300

    Implement Tournament Selection

--- src/main/java/Population.java
+++ src/main/java/Population.java
@@ -5,8 +5,11 @@ public class Population {
     private Board[] boards;
     private double avgFitness;
 
+    private final Random random;
+
     public Population(int[][] initialBoard, int populationSize, Random random) {
         this.boards = new Board[populationSize];
+        this.random = random;
 
         for (int i = 0; i < populationSize; i++) {
             this.boards[i] = new Board(initialBoard, random);
@@ -20,6 +23,22 @@ public class Population {
             this.avgFitness += board.getFitness();
         }
         this.avgFitness = this.avgFitness / this.boards.length;
+    }
+
+    public Board tournamentSelection(double rate) {
+
+        int competitors = (int) (rate * this.boards.length);
+        Board winner = null;
+
+        for (int i = 0; i < competitors; i++) {
+            Board competitor = this.boards[this.random.nextInt(this.boards.length)];
+
+            if (winner == null || competitor.getFitness() > winner.getFitness()) {
+                winner = competitor;
+            }
+        }
+
+        return winner;
     }
 
     public Board[] getBoards() {
